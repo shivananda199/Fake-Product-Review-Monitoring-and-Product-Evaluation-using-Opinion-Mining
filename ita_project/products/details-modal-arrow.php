9c08a235b0b649993dccb56f8a407a59745e99b1
Shivananda D <shivananda199@gmail.com>
2018-02-16 03:40:13 +0530

    added project files

---
+++ ita_project/products/details-modal-arrow.php
@@ -0,0 +1,50 @@
+<div class="modal fade details-7" id="details-7" tableindex="-1" role="dialog" aria-labelledby="details-7" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title text-center">Arrow shirt</h4>
+				</div>
+				<div class="modal-body">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-sm-6">
+								<div class="center-block">
+									<img src="images/Arrow_grey.jpg" alt="Arrow" class=details img-responsive" />
+								</div>
+							</div>
+                       <div class="col-sm-6">
+                       	<h4>Details</h4>
+                       	<p>This product is from Arrow and is amazing</p>
+                       	<hr />
+                       	<p>Price: 2500/-</p>
+                       	<p>Brand: Arrow</p>
+                          <form action="/action_page.php">
+                          	Review: <input type="text" name="Review"><br>
+
+                     <!--  	<form action="add_cart.php" method="post">
+                       		<div class="form-group">
+                       			<div class="col-xs-3">
+                       				<label for="quantity" id="quantity-label">Quantity:</label>
+                       			</div><br />
+                       			<div class="form-group">
+                                <label for="size">Size:</label>
+                            </div>
+                        </div>
+                    </form> -->
+                </form>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+</div>
+
+
+
+
+
+
+
---
+++ ita_project/products/details-modal-levis-blue.php
@@ -0,0 +1,50 @@
+<div class="modal fade details-1" id="details-1" tableindex="-1" role="dialog" aria-labelledby="details-1" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title text-center">levis shirt</h4>
+				</div>
+				<div class="modal-body">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-sm-6">
+								<div class="center-block">
+									<img src="images/blue_shirt.jpeg" alt="Levis blue shirt" class=details img-responsive" />
+								</div>
+							</div>
+                       <div class="col-sm-6">
+                       	<h4>Details</h4>
+                       	<p>This product is from levis and is amazing</p>
+                       	<hr />
+                       	<p>Price: 1500/-</p>
+                       	<p>Brand: Levis</p>
+                          <form action="/action_page.php">
+                          	Review: <input type="text" name="Review"><br>
+
+                     <!--  	<form action="add_cart.php" method="post">
+                       		<div class="form-group">
+                       			<div class="col-xs-3">
+                       				<label for="quantity" id="quantity-label">Quantity:</label>
+                       			</div><br />
+                       			<div class="form-group">
+                                <label for="size">Size:</label>
+                            </div>
+                        </div>
+                    </form> -->
+                </form>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+</div>
+
+
+
+
+
+
+
